bceb28649e94665ac34dbcbd5d703f4e66d978b0
Rodrigo Martins <rodrigo.matias.martins@live.com>
2024-03-14 16:51:18 -0300

    Add project files.

---
+++ Models/Premium.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Project.Models
+{
+    public class Premium
+    {
+        [Key]
+        [DisplayName("Id")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Informe o titulo do Premium")]
+        [StringLength(69, ErrorMessage = "O nome deve conter ate 69 caracteres")]
+        [MinLength(5, ErrorMessage = "O nome deve conter no minimo 5 caracteres")]
+        [DisplayName("Titulo")]
+        public string Title { get; set; } = string.Empty;
+
+        [DataType(DataType.DateTime)]
+        //[GreaterThanToday]
+        [DisplayName("Inicio")]
+        public DateTime StarDate { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayName("Termino")]
+        public DateTime EndDate { get; set; }
+
+        [DisplayName("Aluno")]
+        [Required(ErrorMessage = "Aluno invalido")]
+        public int StudentId { get; set; }
+
+        public Student? Student { get; set; }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project.Models;
+
+public class Student
+{
+    [Key]
+    [DisplayName("Id")]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Informe o nome")]
+    [StringLength(100, ErrorMessage = "O nome deve conter ate 80 caracteres")]
+    [MinLength(5, ErrorMessage = "O nome deve conter no minimo 5 caracteres")]
+    [DisplayName("Nome Completo")]
+    public string Name { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Informe o nome")]
+    [EmailAddress(ErrorMessage = "E-mail invalido")]
+    [DisplayName("E-mail")]
+    public string Email { get; set; } = string.Empty;       
+
+    public List<Premium> Premiums { get; set; } = new();
+}
